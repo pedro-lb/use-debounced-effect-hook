665de5b081f61fcf959706014d0cdb784d75f64e
Pedro Bini <dev.pedrobini@gmail.com>
2020-02-17 09:19:38 -0300

    Improve useDeepCompareMemoize

--- package/src/useDebouncedEffect.js
+++ package/src/useDebouncedEffect.js
@@ -17,8 +17,6 @@ function deepCompareEquals(a, b) {
  */
 function useDeepCompareMemoize(value) {
   const ref = useRef();
-  // it can be done by using useMemo as well
-  // but useRef is rather cleaner and easier
 
   if (!deepCompareEquals(value, ref.current)) {
     ref.current = value;
